7d0762636cf5a20739cf5efbe06650a57ab528d8
jliem123 <31353232+jliem123@users.noreply.github.com>
2017-10-30 11:53:57 -0700

    1

---
+++ Student.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Student here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Student
+{
+    /**
+     * Constructor for objects of class Student
+     */
+    
+    private String name;
+    private int idNumber;
+    private Student bestFriend;
+    
+    public Student()
+    {
+          
+    }
+    
+    public Student(int num, String nm) {
+        this.idNumber = num;
+        this.name = nm;
+    }
+    
+    public int combineIDNumbers() {
+        return this.idNumber + bestFriend.idNumber;
+    }
+    
+    public Student newStudent(Student other) {
+        Student temp = new Student(other.idNumber, this.name);
+        return temp;
+    }
+    
+    public int getID() {
+        return this.idNumber;
+    }
+   
+    public String toString() {
+        return "The student's name is " + this.name;
+    }
+}
---
+++ StudentDriver.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class StudentDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StudentDriver
+{
+   public static void main(String[] args) {
+       Student sam = new Student(1234, "Sam");
+       Student max = new Student(5678, "Max");
+       System.out.println(sam);
+       System.out.println(max);
+    }
+}
